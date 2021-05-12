173f4b7da820faf37980290abbb6f9a43ee7bcb4
tenzin1308 <tenzin1308>
2021-05-12 18:36:22 +0500

    taboo list backend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
+import tabooRouter from './routers/tabooRouter.js';
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/futuretech', {
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/taboo', tabooRouter);
 app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
