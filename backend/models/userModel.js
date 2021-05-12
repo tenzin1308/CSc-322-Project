3b8fc3c0aa948a5d4d96cd6c485c9d5c986a6619
tenzin1308 <tenzin1308>
2021-05-12 17:54:48 +0500

    implement user home address

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema(
     isBlocked: { type: Boolean, default: false, required: true },
     isSeller: { type: Boolean, default: false, required: true },
     isShipper: { type: Boolean, default: false, required: true },
-    shipper :{
+    shipper: {
       logo: String,
     },
     seller: {
@@ -19,10 +19,18 @@ const userSchema = new mongoose.Schema(
       rating: { type: Number, default: 0, required: true },
       numReviews: { type: Number, default: 0, required: true },
     },
+    homeAddress: {
+      address: { type: String, default: "" },
+      city: { type: String, default: "" },
+      postalCode: { type: String, default: "" },
+      country: { type: String, default: "" },
+      lat: Number,
+      lng: Number,
+    },
   },
   {
     timestamps: true,
   }
 );
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
