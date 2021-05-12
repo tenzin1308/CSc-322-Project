b4f46656a0dd84afcb8edd7d204ffe8c0306e94f
tenzin1308 <tenzin1308>
2021-05-12 09:31:58 +0500

    update shippingStatus from api

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -165,7 +165,7 @@ orderRouter.put(
 
 orderRouter.put(
   "/:id/select-shipper",
-  // isAuth,
+  isAuth,
   expressAsyncHandler(async (req, res) => {
     const { shipperId, price, justification } = req.body;
     const order = await Order.findById(req.params.id);
@@ -173,6 +173,7 @@ orderRouter.put(
       order.shipper = await User.findById(shipperId);
       order.shippingPrice = price;
       order.selectShipperJustification = justification;
+      order.shippingStatus = "Shipping";
 
       const updatedOrder = await order.save();
       res.send({ message: "Select ShipperS Success", order: updatedOrder });
